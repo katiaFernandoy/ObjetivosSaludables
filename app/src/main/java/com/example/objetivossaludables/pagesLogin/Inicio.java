9d4baf88cfc284e60bebe1683a32158b55a99778
katiaFernandoy <madrid522@hotmail.com>
2023-04-10 00:20:52 +0200

    Patch - cambios en el inicio de la aplicación

---
+++ app/src/main/java/com/example/objetivossaludables/pagesLogin/Inicio.java
@@ -0,0 +1,35 @@
+package com.example.objetivossaludables.pagesLogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.objetivossaludables.R;
+
+public class Inicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inicio);
+
+        //new HiloEsperaCount().start();
+    }
+
+    class HiloEsperaCount extends Thread {
+
+        @Override
+        public void run() {
+            try {
+                sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            Intent intent = new Intent(Inicio.this, Login.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+}
