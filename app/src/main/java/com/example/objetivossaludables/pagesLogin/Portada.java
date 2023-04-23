024ada7b59b9093c83e5191e05cbd371b5520069
KatiaFernandoy <madrid522@hotmail.com>
2023-04-23 20:13:23 +0200

    Patch - cambios cambios

---
+++ app/src/main/java/com/example/objetivossaludables/pagesLogin/Portada.java
@@ -0,0 +1,35 @@
+package com.example.objetivossaludables.pagesLogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.objetivossaludables.R;
+
+public class Portada extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_portada);
+
+        findViewById(R.id.bt_crearCuentaPortada).setOnClickListener(v -> {
+            Intent intent = new Intent(Portada.this,CrearCuenta.class);
+            startActivity(intent);
+        });
+
+        findViewById(R.id.bt_iniciarSesionPortada).setOnClickListener(v -> {
+            Intent intent = new Intent(Portada.this,IniciarSesion.class);
+            startActivity(intent);
+        });
+
+
+
+
+
+
+    }
+}
