0a45435862438ba1cf04b5e5b60fdec29292d458
KatiaFernandoy <madrid522@hotmail.com>
2023-04-30 03:16:59 +0200

    Patch - creacion de la nuevo pantalla home menu

---
+++ app/src/main/java/com/example/objetivossaludables/pagesLogin/Menu.java
@@ -0,0 +1,16 @@
+package com.example.objetivossaludables.pagesLogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.objetivossaludables.R;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
