cd623fd8edf738acf16f9cd8f76f4932615a8f4d
KatiaFernandoy <madrid522@hotmail.com>
2023-05-07 21:00:36 +0200

    Patch - agregados String con getters y setters

--- app/src/main/java/com/example/objetivossaludables/modelo/Noticias.java
+++ app/src/main/java/com/example/objetivossaludables/modelo/Noticias.java
@@ -3,10 +3,12 @@ package com.example.objetivossaludables.modelo;
 public class Noticias {
     private String titulo;
     private  int titleImage;
+    private String link_btn;
 
-    public Noticias(String titulo, int titleImage) {
+    public Noticias(String titulo, int titleImage,String link_btn) {
         this.titulo = titulo;
         this.titleImage = titleImage;
+        this.link_btn = link_btn;
     }
 
     public String getTitulo() {
@@ -24,4 +26,14 @@ public class Noticias {
     public void setTitleImage(int titleImage) {
         this.titleImage = titleImage;
     }
+
+    public String getLink_btn() {
+        return link_btn;
+    }
+
+    public void setLink_btn(String link_btn) {
+        this.link_btn = link_btn;
+    }
 }
+
+
