fefeb38e1b84c04a6c5dd63d515cbb5d6af59d11
KatiaFernandoy <madrid522@hotmail.com>
2023-05-26 00:05:15 +0200

    Patch - seteando staticos con valores por defecto

---
+++ app/src/main/java/com/example/objetivossaludables/valoresestaticos/SharedPreferences.java
@@ -0,0 +1,15 @@
+package com.example.objetivossaludables.valoresestaticos;
+
+import java.security.PublicKey;
+
+public class SharedPreferences {
+
+    public static final String MY_PREFERENCES = "MyPrefs";
+    public static final String STATUS = "status";
+    public static final String EMAIL = "email";
+    public static final String PASSWORD = "password";
+    public static final String USERNAME = "username";
+
+    public static final String ID = "id";
+
+}
---
+++ app/src/main/java/com/example/objetivossaludables/valoresestaticos/URLs.java
@@ -0,0 +1,8 @@
+package com.example.objetivossaludables.valoresestaticos;
+
+public class URLs {
+
+    private static final String URL = "https://objetivossaludablesks.000webhostapp.com/";
+    public static final String URL_LOGIN = URL + "login.php";
+    public static final String URL_REGISTRO = URL + "registro.php";
+}
