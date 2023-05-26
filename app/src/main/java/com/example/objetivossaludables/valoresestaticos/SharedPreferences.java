9859a6ad049cbc1dbe768eb26b9ac04f86296d97
Katia Fernandoy <madrid522@gmail.com>
2023-05-26 16:23:08 +0200

    Revert "Patch - borrando import"
    This reverts commit 8b47c113c18dca0c3f257931aa04c0d850d2fadc.

--- app/src/main/java/com/example/objetivossaludables/valoresestaticos/SharedPreferences.java
+++ app/src/main/java/com/example/objetivossaludables/valoresestaticos/SharedPreferences.java
@@ -7,6 +7,7 @@ public class SharedPreferences {
     public static final String EMAIL = "email";
     public static final String PASSWORD = "password";
     public static final String USERNAME = "username";
+
     public static final String ID = "id";
 
 }
