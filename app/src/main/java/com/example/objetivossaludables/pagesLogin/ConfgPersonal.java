2002899100395ff048f6353f7beb81cefa0c111b
KatiaFernandoy <madrid522@hotmail.com>
2023-05-28 21:22:14 +0200

    Patch - creacion de atrubutos y metodo de iniciar spinner

--- app/src/main/java/com/example/objetivossaludables/pagesLogin/ConfgPersonal.java
+++ app/src/main/java/com/example/objetivossaludables/pagesLogin/ConfgPersonal.java
@@ -3,16 +3,41 @@ package com.example.objetivossaludables.pagesLogin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.example.objetivossaludables.R;
+import com.example.objetivossaludables.adapters.CustomSpinnerAdapters;
+
+import java.util.Arrays;
 
 public class ConfgPersonal extends AppCompatActivity {
+
+    private EditText txt_nombreModif;
+    private EditText txt_PesoModif;
+    private EditText txt_fechaModif;
+    private Spinner spn_generoModif;
+    private EditText txt_alturaModif;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confg_personal);
+        iniciarSpinner();
 
+
+    }
+
+    private void iniciarSpinner() {
+
+        spn_generoModif = findViewById(R.id.spinner);
+
+        CustomSpinnerAdapters adapters = new CustomSpinnerAdapters(
+                this, Arrays.asList(getResources().getStringArray(R.array.spinnerGenero)));
+
+        spn_generoModif.setAdapter(adapters);
 
     }
 }
