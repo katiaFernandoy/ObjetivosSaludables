006c714865663bf4cf000811dcc32e468c2b8c00
KatiaFernandoy <madrid522@hotmail.com>
2023-06-02 00:34:51 +0200

    Patch - añadidos nuevos values

---
+++ app/src/main/java/com/example/objetivossaludables/valoresestaticos/ValuesPreferences.java
@@ -0,0 +1,21 @@
+package com.example.objetivossaludables.valoresestaticos;
+
+public class ValuesPreferences {
+
+    public static final String MY_PREFERENCES = "MyPrefs";
+    public static final String STATUS = "status";
+    public static final String USER_ID= "userId";
+    public static final String EMAIL = "email";
+    public static final String PASSWORD = "password";
+    public static final String NOMBRE = "nombre";
+    public static final String APELLIDO = "apellido";
+
+    // AJUSTES
+    public static final String SONIDO = "sonido";
+    public static final String VIBRACION = "vibracion";
+    public static final String IDIOMA = "idioma";
+
+    public static final String COLOR_1 = "#4CAF50";
+    public static final String COLOR_2 = "#A4EFA7";
+
+}
