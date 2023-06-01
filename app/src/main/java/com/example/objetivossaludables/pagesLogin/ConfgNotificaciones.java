1313caea30faddbc4127d3c18699dc4d393556ec
KatiaFernandoy <madrid522@hotmail.com>
2023-06-01 17:53:24 +0200

    Patch - creada clase para las notificaciones y clase seguridad, apiModificar ,añadidos colores, creada clase usuario

---
+++ app/src/main/java/com/example/objetivossaludables/pagesLogin/ConfgNotificaciones.java
@@ -0,0 +1,16 @@
+package com.example.objetivossaludables.pagesLogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.objetivossaludables.R;
+
+public class ConfgNotificaciones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confg_notificaciones);
+    }
+}
