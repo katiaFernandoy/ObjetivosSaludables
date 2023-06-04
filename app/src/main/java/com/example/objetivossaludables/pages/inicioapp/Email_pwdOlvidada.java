ab1a2e92df53fe785c5193a8bc7b8f6b8dbd25d8
KatiaFernandoy <madrid522@hotmail.com>
2023-06-05 01:51:39 +0200

    Patch - paginas añadidas

---
+++ app/src/main/java/com/example/objetivossaludables/pages/inicioapp/Email_pwdOlvidada.java
@@ -0,0 +1,16 @@
+package com.example.objetivossaludables.pages.inicioapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.objetivossaludables.R;
+
+public class Email_pwdOlvidada extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email_pwd_olvidada);
+    }
+}
