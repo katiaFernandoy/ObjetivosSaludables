6e85c274723a6dc2a11fd12bdc78df7a46e09b96
KatiaFernandoy <madrid522@hotmail.com>
2023-06-05 02:30:43 +0200

    major - refactor ConfgPersonal.java

--- app/src/main/java/com/example/objetivossaludables/pages/configuracion/ConfgPersonal.java
+++ app/src/main/java/com/example/objetivossaludables/pages/configuracion/ConfgPersonal.java
@@ -1,10 +1,12 @@
 package com.example.objetivossaludables.pages.configuracion;
 
-import android.annotation.SuppressLint;
-import android.app.ProgressDialog;
-import android.content.Context;
+import static com.example.objetivossaludables.valoresestaticos.ParametrosHashMap.getParamsInfoPersonal;
+import static com.example.objetivossaludables.valoresestaticos.URLs.URL_INFORMACION_PERSONAL;
+import static com.example.objetivossaludables.valoresestaticos.URLs.URL_MODIFICAR_INFO_PERSONAL;
+import static com.example.objetivossaludables.valoresestaticos.ValuesPreferences.DATE_FORMAT;
+import static com.example.objetivossaludables.valoresestaticos.Verificaciones.*;
+
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -14,78 +16,79 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.objetivossaludables.R;
 import com.example.objetivossaludables.adapters.CustomSpinnerAdapters;
-import com.example.objetivossaludables.manager.api.ApiGetInformacionPersonal;
-import com.example.objetivossaludables.manager.api.ApiModificarInfoPersonal;
+import com.example.objetivossaludables.manager.api.ApiHandler;
+import com.example.objetivossaludables.manager.api.ApiInterface;
+import com.example.objetivossaludables.manager.progressdialog.PdLoading;
 import com.example.objetivossaludables.manager.sharedpreferences.UserPreferences;
 import com.example.objetivossaludables.modelo.InformacionPersonal;
 import com.google.android.material.textfield.TextInputEditText;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.ParseException;
 import java.util.Arrays;
+import java.util.HashMap;
 
-public class ConfgPersonal extends AppCompatActivity {
+public class ConfgPersonal extends AppCompatActivity implements ApiInterface {
 
-    private static InformacionPersonal infoPersonal = null;
-    protected static Spinner spn_generoModif;
-    protected static TextInputEditText txt_nombreMod, txt_pesoMod, txt_fechaMod, txt_alturaMod;
-    @SuppressLint("SimpleDateFormat")
-    protected static UserPreferences preferences;
-    protected static ProgressDialog pdLoading;
-
-    public static Context context;
-
-    protected static InformacionPersonal getInfoPersonal() {
-        return infoPersonal;
-    }
-
-    protected static void setInfoPersonal(InformacionPersonal infoPersonal) {
-        ConfgPersonal.infoPersonal = infoPersonal;
-    }
+    private InformacionPersonal oldInfo, newInfo;
+    private boolean isObtenerInfo;
+    private Spinner spn_generoModif;
+    private TextInputEditText txt_nombreMod, txt_pesoMod, txt_fechaMod, txt_alturaMod;
+    private UserPreferences preferences;
+    private PdLoading pdLoading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confg_personal);
-        context = ConfgPersonal.this;
+
         preferences = new UserPreferences(this);
-        pdLoading = new ProgressDialog(this);
-
         iniciarSpinner();
-
+        TextView txt_mailInfoUsu = findViewById(R.id.txt_emailInfoPersonal);
+        txt_mailInfoUsu.setText(preferences.getUserEmail());
         txt_nombreMod = findViewById(R.id.txt_nombreModif);
         txt_pesoMod = findViewById(R.id.txt_pesoModif);
         txt_fechaMod = findViewById(R.id.txt_fechaModif);
         txt_alturaMod = findViewById(R.id.txt_alturaModif);
-        TextView txt_mailInfoUsu = findViewById(R.id.txt_emailInfoPersonal);
-        txt_mailInfoUsu.setText(preferences.getUserEmail());
         Button btn_modificar = findViewById(R.id.bt_modificarInfoUsu);
 
-        new ApiGetInformacionPersonal();
+        obtenerInformacionPersonal();
 
-        btn_modificar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(!verificarDatos()){
-                    return;
-                }
-                new ApiModificarInfoPersonal();
+        btn_modificar.setOnClickListener(v -> {
+            if(!verificarDatos()){
+                return;
+            }
+            try {
+                nuevaInformacionPersonal();
+            } catch (ParseException e) {
+                throw new RuntimeException(e);
             }
         });
     }
 
     public boolean verificarDatos(){
-        if(txt_nombreMod.getText() == null || txt_nombreMod.getText().equals("")){
-            Toast.makeText(context.getApplicationContext(),getResources().getText(R.string.errorFaltaCampo),Toast.LENGTH_SHORT).show();
+        if(txt_nombreMod.getText() == null || getTexto(txt_nombreMod).equals("")){
+            Toast.makeText(this,getResources().getText(R.string.faltaCampoNombre),Toast.LENGTH_SHORT).show();
             return false;
-        }if(txt_fechaMod.getText() == null || txt_fechaMod.getText().equals("")){
-            Toast.makeText(context.getApplicationContext(),getResources().getText(R.string.campoFecha),Toast.LENGTH_SHORT).show();
+        }if(txt_pesoMod.getText() == null || getTexto(txt_pesoMod).equals("")){
+            Toast.makeText(this,getResources().getText(R.string.faltaCampoPeso),Toast.LENGTH_SHORT).show();
             return false;
-        }if(txt_pesoMod.getText() == null || txt_pesoMod.getText().equals("")){
-            Toast.makeText(context.getApplicationContext(),getResources().getText(R.string.errorFaltaCampo),Toast.LENGTH_SHORT).show();
+        }if(!isDouble(getTexto(txt_pesoMod))){
+            Toast.makeText(this,getResources().getText(R.string.campoPesoIncorrecto),Toast.LENGTH_SHORT).show();
             return false;
-        }if(txt_alturaMod.getText() == null || txt_pesoMod.getText().equals("")){
-            Toast.makeText(context.getApplicationContext(),getResources().getText(R.string.campoAltura),Toast.LENGTH_SHORT).show();
+        }if(txt_fechaMod.getText() == null || getTexto(txt_fechaMod).equals("")){
+            Toast.makeText(this,getResources().getText(R.string.campoFecha),Toast.LENGTH_SHORT).show();
+            return false;
+        }if(!isDate(getTexto(txt_fechaMod))){
+            Toast.makeText(this,getResources().getText(R.string.campoFechaIncorrecto),Toast.LENGTH_SHORT).show();
+            return false;
+        }if(txt_alturaMod.getText() == null || getTexto(txt_alturaMod).equals("")){
+            Toast.makeText(this,getResources().getText(R.string.campoAltura),Toast.LENGTH_SHORT).show();
+            return false;
+        }if(!isInteger( getTexto(txt_alturaMod))){
+            Toast.makeText(this,getResources().getText(R.string.campoAlturaIncorrecto),Toast.LENGTH_SHORT).show();
             return false;
         }
         return true;
@@ -100,9 +103,96 @@ public class ConfgPersonal extends AppCompatActivity {
         spn_generoModif.setAdapter(adapters);
     }
 
+    private void obtenerInformacionPersonal(){
+        isObtenerInfo = true;
+        pdLoading = new PdLoading(this);
+        new ApiHandler(this,URL_INFORMACION_PERSONAL,
+                getParamsInfoPersonal(preferences.getUserEmail())).start();
+    }
+
+    private void nuevaInformacionPersonal() throws ParseException {
+        pdLoading = new PdLoading(this);
+        new ApiHandler(this,URL_MODIFICAR_INFO_PERSONAL, settingInformacionPersonal()).start();
+    }
+
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        infoPersonal = null;
+    public void returnResponse(JSONObject json) {
+        pdLoading.dismiss(); // Se cierra el PdLoading
+
+        try {
+            if(isObtenerInfo){
+                handlerResponseObtenerInfoPersonal(json);
+                return;
+            }
+
+            if (json.getBoolean("error")) {
+                Toast.makeText(this, getResources().getText(R.string.errorAlObtenerInfo), Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            Toast.makeText(this, json.getString("message"), Toast.LENGTH_SHORT).show();
+            oldInfo = newInfo;
+            newInfo = null;
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void handlerResponseObtenerInfoPersonal(JSONObject json) throws JSONException, ParseException {
+        isObtenerInfo = false;
+
+        if(json.getBoolean("error")){
+            Toast.makeText(this, getResources().getText(R.string.errorAlObtenerInfo), Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        Toast.makeText(this,json.getString("message"),Toast.LENGTH_SHORT).show();
+        oldInfo = new InformacionPersonal(
+                preferences.getUserEmail(),
+                json.getString("nombre"),
+                json.getDouble("peso"),
+                json.getInt("altura"),
+                json.getString("genero"),
+                DATE_FORMAT.parse(json.getString("fechaNacimiento")));
+
+        runOnUiThread(() -> {
+            txt_nombreMod.setText(oldInfo.getNombre());
+            txt_pesoMod.setText(String.valueOf(oldInfo.getPeso()));
+            txt_fechaMod.setText(DATE_FORMAT.format(oldInfo.getFechaNacimiento()));
+            spn_generoModif.setSelection(getGenero(oldInfo.getGenero()));
+            txt_alturaMod.setText(String.valueOf(oldInfo.getAltura()));
+        });
+    }
+
+    private HashMap<String,String> settingInformacionPersonal() throws ParseException {
+        newInfo = new InformacionPersonal(
+                oldInfo.getEmail(),
+                getTexto(txt_nombreMod),
+                Double.parseDouble(getTexto(txt_pesoMod)),
+                Integer.parseInt(getTexto(txt_alturaMod)),
+                spn_generoModif.getSelectedItem().toString(),
+                DATE_FORMAT.parse(getTexto(txt_fechaMod)));
+
+        return getParamsInfoPersonal(
+                newInfo.getEmail(),
+                newInfo.getNombre().equals(oldInfo.getNombre()) ? "" : newInfo.getNombre(),
+                newInfo.getPeso() == oldInfo.getPeso() ? "" : String.valueOf(newInfo.getPeso()),
+                newInfo.getAltura() == oldInfo.getAltura() ? "" : String.valueOf(newInfo.getAltura()),
+                DATE_FORMAT.format(oldInfo.getFechaNacimiento()).equals(getTexto(txt_fechaMod)) ? "" : getTexto(txt_fechaMod),
+                newInfo.getGenero().equals(oldInfo.getGenero()) ? "" : newInfo.getGenero());
+    }
+
+    private int getGenero(String genero) {
+        String[] generos = getResources().getStringArray(R.array.spinnerGenero);
+
+        for (int i = 0; i < generos.length; i++) {
+            if (generos[i].equals(genero)) {
+                return i;
+            }
+        }
+
+        return 0;
     }
 }
--- app/src/main/java/com/example/objetivossaludables/valoresestaticos/ParametrosHashMap.java
+++ app/src/main/java/com/example/objetivossaludables/valoresestaticos/ParametrosHashMap.java
@@ -6,10 +6,28 @@ import java.util.HashMap;
 
 public class ParametrosHashMap {
 
+    public static HashMap<String,String> getParamsInfoPersonal(String email){
+        return new HashMap<String,String>(){{
+            put(EMAIL,email);
+        }};
+    }
+
     public static HashMap<String,String> getParamsInfoPersonal(
             String email, String peso, String altura, String fechaNacimiento, String genero){
         return new HashMap<String,String>(){{
             put(EMAIL,email);
+            put("peso",peso);
+            put("altura",altura);
+            put("fechaNacimiento",fechaNacimiento);
+            put("genero",genero);
+        }};
+    }
+
+    public static HashMap<String,String> getParamsInfoPersonal(
+            String email, String nombre, String peso, String altura, String fechaNacimiento, String genero){
+        return new HashMap<String,String>(){{
+            put(EMAIL,email);
+            put("nombre",nombre);
             put("peso",peso);
             put("altura",altura);
             put("fechaNacimiento",fechaNacimiento);
