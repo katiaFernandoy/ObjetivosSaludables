f1e0bcf52d2112c486c5bb7be33d205591d76003
katiaFernandoy <130239048+katiaFernandoy@users.noreply.github.com>
2023-06-14 01:04:20 +0200

    Merge pull request #32
    * Patch - añadidos estilos ,boton y nuevos Strings
    
    * Patch - añadidos metodos nuevos borrar cuenta
    
    * Merge remote-tracking branch 'origin/master' into katia
    
    * Merge remote-tracking branch 'origin/master' into katia
    
    * Merge remote-tracking branch 'origin/master' into katia
    
    * Patch - nuevo boton eliminar cuenta y cambios de nombres
    
    * Merge remote-tracking branch 'origin/master' into katia
    
    * Patch - cosas de enviar email para comentarios
    
    * Patch - cosas de enviar email para comentarios
    
    * Merge remote-tracking branch 'origin/master' into katia
    
    * Merge remote-tracking branch 'origin/katia' into katia
    
    * patch - añadido enviar mail con un comentario

---
+++ app/src/main/java/com/example/objetivossaludables/pages/configuracion/ConfgEnviar_Comentarios.java
@@ -0,0 +1,51 @@
+package com.example.objetivossaludables.pages.configuracion;
+
+
+import static com.example.objetivossaludables.valoresestaticos.Verificaciones.getTexto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Looper;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.objetivossaludables.R;
+import com.example.objetivossaludables.manager.mailManager.MailJob;
+import com.example.objetivossaludables.manager.progressdialog.PdLoading;
+import com.example.objetivossaludables.manager.sharedpreferences.UserPreferences;
+import com.google.android.material.textfield.TextInputEditText;
+
+public class ConfgEnviar_Comentarios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.enviarcomentarios);
+
+        findViewById(R.id.backNavigationButton).setOnClickListener(v -> onBackPressed());
+        UserPreferences preferences = new UserPreferences(this);
+
+        Button bt_enviarComentario = findViewById(R.id.bt_enviarComentario);
+        TextInputEditText txt_escribirComentario = findViewById(R.id.txt_escribirComentario);
+
+        bt_enviarComentario.setOnClickListener(v -> {
+            String comentario = getTexto(txt_escribirComentario);
+
+            if(comentario.isEmpty()){
+                Toast.makeText(this, "Por favor rellena el contenido", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            PdLoading pdLoading = new PdLoading(this);
+
+            new Thread(() -> {
+                Looper.prepare();
+                new MailJob(preferences.getUserEmail(),comentario);
+                pdLoading.dismiss();
+                Toast.makeText(this,"Mensaje enviado correctamente",Toast.LENGTH_SHORT).show();
+            }).start();
+        });
+
+    }
+}
